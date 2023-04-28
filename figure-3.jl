aa6e54c0bb4310457b07f956a1bfb78a6f48e7d2
Robert Gowers <robert.gowers@hu-berlin.de>
2023-04-28 10:38:46 +0200

    added inset plot to bifurcation figure

--- figure-3.jl
+++ figure-3.jl
@@ -56,20 +56,33 @@ xlabel("\$I_{\\mathrm{ext}}\$ (pA)", fontsize=font_axis)
 ylabel("\$G_{\\mathrm{in}}\$ (nS)", fontsize=font_axis)
 title("Bifurcation diagram", fontsize=font_title)
 
+data_in = BSON.load("data/MLDS-bif-bt.bson")
+τδ_in = data_in[:τδ]
+gbt_in = data_in[:gbt]
+Ibt_in = data_in[:Ibt] 
+τbtc = data_in[:τbtc]
+_, idx_btc = findmin(abs.(τδ_in.-τbtc[1]))
+τmax = 20.0
+idx_max = findfirst.(isequal.(τmax), (τδ_in,))
+
 ax.spines["right"].set_visible(false)
 ax.spines["top"].set_visible(false)
 
-# axin = ax.inset_axes([0.72, 0.075, 0.4, 0.4])
-# axin.plot(Isn[:,1], gin_DS, color="k", alpha=alpha, label="SN")
-# axin.plot(Isn[:,2], gin_DS, color="k", alpha=alpha)
-# axin.plot(Ibt_S, gbt_S, "o", color=colors[1], alpha=alpha)
-# for j in eachindex(τδ)
-#   axin.plot(Ibt_DS[j], gbt_DS[j], "o", color=colors[j+1], alpha=0.5)
-# end
-# axin.set_xlim(140.0, 175.0)
-# axin.set_ylim(4.7, 5.6)
-# axin.spines["right"].set_visible(false)
-# axin.spines["top"].set_visible(false)
+axin = ax.inset_axes([0.75, 0.16, 0.3, 0.3])
+
+axin.plot(τbtc[1],	gc, "^", color="k", alpha=alpha, label="Cusp")
+axin.plot(τδ_in[1:idx_btc-1], gbt_in[1:idx_btc-1], linewidth = linewidth, color="k", label="BT (upper)")
+axin.plot(τδ_in[idx_btc:idx_max[1]], gbt_in[idx_btc:idx_max[1]], "--", linewidth = linewidth, color="k", label="BT (lower)")
+
+axin.plot(0.0, gbt_S, "o", color=colors[1], alpha=alpha)
+for j in eachindex(τδ)
+	axin.plot(τδ[j],	gbt_DS[j], "o", color=colors[j+1], alpha=0.5)
+end
+axin.set_xlabel("\$\\tau_{\\delta}\$ (ms)", fontsize=font_legend)
+axin.set_ylabel("\$G_{\\mathrm{in}}^{\\mathrm{BT}}\$ (nS)", fontsize=font_legend)
+
+axin.spines["right"].set_visible(false)
+axin.spines["top"].set_visible(false)
 
 savefig("figures/figure-3.svg", bbox_inches="tight")
 show()
